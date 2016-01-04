f4295162f26f76339d171c33b52b743cbe3c346b
DrPsycho <drpsychoz@gmail.com>
2016-01-04 17:52:32 +0600

    chaged input param

--- littlegame.go
+++ littlegame.go
@@ -8,20 +8,24 @@ import (
 	"net/http"
 )
 
-var addr = flag.String("addr", "10.0.0.4:8080", "http service address")
-var homeTempl = template.Must(template.ParseFiles("/home/drpsycho/js/jsgame/sandbox.html"))
-var upgrader = websocket.Upgrader{} // use default options
+var addr = flag.String("addr", "localhost:8080", "http service address")
+var workdir = flag.String("workdir", "./", "work dir")
+
+var homeTempl *template.Template
+var upgrader = websocket.Upgrader{}
 
 func handler(rw http.ResponseWriter, req *http.Request) {
 	homeTempl.Execute(rw, "ws://"+req.Host+"/handler")
 }
 
 func main() {
+	flag.Parse()
 
+	homeTempl = template.Must(template.ParseFiles(*workdir+"/sandbox.html"))
 	go HubHandler.run()
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/handler", serverWs)
-	http.Handle("/stuff/", http.StripPrefix("/stuff/", http.FileServer(http.Dir("/home/drpsycho/js/jsgame/stuff/"))))
+	http.Handle("/stuff/", http.StripPrefix("/stuff/", http.FileServer(http.Dir(*workdir+"/stuff/"))))
 
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
